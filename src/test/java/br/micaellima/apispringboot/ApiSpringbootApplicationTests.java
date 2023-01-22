af11e23a5ba6e72ee3f15307ac2ce7d70c404558
Micael Lima <micael1998lima@gmail.com>
2023-01-22 08:55:19 -0300

    init project

---
+++ src/test/java/br/micaellima/apispringboot/ApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.micaellima.apispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
